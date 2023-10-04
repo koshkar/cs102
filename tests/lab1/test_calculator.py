c83bd4819b71dac29b8998ded191699c853f8b7e
Staff007 <qwerty376l@mail.ru>
2023-10-05 01:31:08 +0300

    Добавлено решение и тесты

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,68 @@
+import tkinter as tk
 import unittest
+import sys
+sys.path.append('/Users/kirillkoskarev/programming_itmo/cs102/src')
+from lab1.calculator import Calculator
 
 class CalculatorTestCase(unittest.TestCase):
 
-    # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+    def test_addition(self):
+        calculator = Calculator(tk.Tk())
+        calculator.button_5.invoke()
+        calculator.add_button.invoke()
+        calculator.button_7.invoke()
+        calculator.equal_button.invoke()
+        result = calculator.number_entry.get()
+        self.assertEqual(result, "12")
+
+    def test_substruction(self):
+        calculator = Calculator(tk.Tk())
+        calculator.button_5.invoke()
+        calculator.subtract_button.invoke()
+        calculator.button_7.invoke()
+        calculator.equal_button.invoke()
+        result = calculator.number_entry.get()
+        self.assertEqual(result, "-2")
+
+    def test_multiplication(self):
+        calculator = Calculator(tk.Tk())
+        calculator.button_8.invoke()
+        calculator.multiply_button.invoke()
+        calculator.button_2.invoke()
+        calculator.equal_button.invoke()
+        result = calculator.number_entry.get()
+        self.assertEqual(result, "16")
+
+    def test_division(self):
+        calculator = Calculator(tk.Tk())
+        calculator.button_8.invoke()
+        calculator.divide_button.invoke()
+        calculator.button_2.invoke()
+        calculator.equal_button.invoke()
+        result = calculator.number_entry.get()
+        self.assertEqual(result, "4.0")
+
+    def test_floor_division(self):
+        calculator = Calculator(tk.Tk())
+        calculator.button_8.invoke()
+        calculator.floor_div_button.invoke()
+        calculator.button_2.invoke()
+        calculator.equal_button.invoke()
+        result = calculator.number_entry.get()
+        self.assertEqual(result, "4")
+
+    def test_moduling(self):
+        calculator = Calculator(tk.Tk())
+        calculator.button_3.invoke()
+        calculator.modulus_button.invoke()
+        calculator.button_2.invoke()
+        calculator.equal_button.invoke()
+        result = calculator.number_entry.get()
+        self.assertEqual(result, "1")
+
+    def test_root_calculation(self):
+        calculator = Calculator(tk.Tk())
+        calculator.button_4.invoke()
+        calculator.sqrt_button.invoke()
+        result = calculator.number_entry.get()
+        self.assertEqual(result, "2")
